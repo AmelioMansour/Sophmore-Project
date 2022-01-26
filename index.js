211076ba474b426d08befe661d0d9fc9c3794f60
AmelioMansourPC <ameliomansour@yahoo.com>
2022-01-26 18:16:48 -0500

    did work on index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const path = require("path");
+const ejs = require("ejs");
+const { request } = require("express");
+const res = require("express/lib/response");
+const req = require("express/lib/request");
+
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.use("/public", express.static(__dirname + "/public"));
+
+//Nav routes
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get("/views/index.ejs", (req, res) => {
+    res.render("index");
+});
+
+app.get("/views/html/about.ejs", (req, res) => {
+    res.render("./html/about.ejs");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
